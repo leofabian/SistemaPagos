71ad206bcd3fa3dbabae705f2748525bfa128c07
leofabian <nelsonleonel17@gmail.com>
2017-09-19 14:35:31 -0300

    Initial commit

---
+++ core/modules/index/view/home/widget-default.php
@@ -0,0 +1,16 @@
+<div class="row">
+	<div class="col-md-12">
+		<h1>¡Bienvenido al sistema de control de pagos!</h1>
+
+
+	
+	<div class="jumbotron">
+		<h2>La hora actual es:</h2>
+		<p></p>
+		<p><center><?php echo date("D M d, Y G:i a"); ?></p></center>
+	</div>
+	
+
+<br><br><br><br><br><br><br><br><br><br>
+	</div>
+</div>
